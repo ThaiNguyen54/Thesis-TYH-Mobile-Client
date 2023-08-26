4491adaeb4483d935985d8e881af1c678c87700f
ThaiNguyen54 <59272463+ThaiNguyen54@users.noreply.github.com>
2023-08-26 22:01:05 +0700

    accept self-signed certificate apis

--- app/src/main/java/com/example/tryyourhair/RetrofitInstance/RetrofitClient.java
+++ app/src/main/java/com/example/tryyourhair/RetrofitInstance/RetrofitClient.java
@@ -1,19 +1,27 @@
 package com.example.tryyourhair.RetrofitInstance;
 
+import com.example.tryyourhair.UnsafeOkHttpClient.UnsafeOkHttpClient;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
 
-//   private static String BASE_URL = "http://192.168.1.2:7001/";
+    // Unsafe OkHttp Client is used for accepting self-signed certificate Nodejs server
+    private static OkHttpClient okHttpClient = UnsafeOkHttpClient.getUnsafeOkHttpClient();
 
-    private static String BASE_URL = "https://geminisoftvn.ddns.net:7001/";
+   private static String BASE_URL = "https://192.168.1.12:7001/";
+
+//    private static String BASE_URL = "https://geminisoftvn.ddns.net:7001/";
 
 //    private static String BASE_URL = "https://mobile-dev-final-project-api.vercel.app/";
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create()).build();
         }
         return retrofit;
