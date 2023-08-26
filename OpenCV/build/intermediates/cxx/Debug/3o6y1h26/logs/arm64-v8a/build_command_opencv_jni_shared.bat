acb4d95048c8aefe5a3ea11481e4a769f897408a
ThaiNguyen54 <59272463+ThaiNguyen54@users.noreply.github.com>
2023-08-26 22:06:06 +0700

    modify base_url of retrofit client

--- OpenCV/build/intermediates/cxx/Debug/3o6y1h26/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ OpenCV/build/intermediates/cxx/Debug/3o6y1h26/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\\Users\\Thai\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "D:\\Study\\Cirriculums\\Mobile-Developement\\TT\\Thesis-class\\Final-Project\\Mobile-Dev-Final-Project\\TryYourHair\\OpenCV\\.cxx\\Debug\\3o6y1h26\\arm64-v8a" ^
+  "D:\\Study\\Thesis\\Thesis-TYH-Mobile-Client\\OpenCV\\.cxx\\Debug\\3o6y1h26\\arm64-v8a" ^
   opencv_jni_shared
